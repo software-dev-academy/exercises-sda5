a74d328cab87731439d6e5d8c7e315864fafde71
Philipp Haller <hallerp@gmail.com>
2019-02-19 14:40:52 +0100

    Add a 'get' method and two variants of 'toString'
    This commits adds two variants of 'toString' in order to learn
    about big-O complexity of different implementations.

--- src/main/java/exercises/LinkedList.java
+++ src/main/java/exercises/LinkedList.java
@@ -44,4 +44,54 @@ class LinkedList {
     public int size() {
         return size;
     }
+
+    /**
+     * Complexity: O(N)
+     */
+    public int get(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            return first.elem;
+        }
+        Node current = first;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        // here, we would like "current" to refer to the
+        // node at the given "index"
+        return current.elem;
+    }
+
+    /**
+     * Returns a nice string with all elements of the list, e.g.,
+     * "[2,6]"
+     *
+     * Complexity: O(N^2)
+     */
+    public String toStringSlow() {
+        String s = "[";
+        for (int i = 0; i < size - 1; i++) {
+            s = s + get(i) + ",";
+        }
+        s = s + get(size - 1) + "]";
+        return s;
+    }
+
+    /**
+     * Goal: O(N) complexity
+     */
+    public String toString() {
+        String s = "[";
+        Node current = first;
+        // want to use while
+        while (current.next != null) {
+            s = s + current.elem + ",";
+            current = current.next;
+        }
+        // here, current.next == null
+        s = s + current.elem + "]";
+        return s;
+    }
 }
--- src/main/java/exercises/TestLinkedList.java
+++ src/main/java/exercises/TestLinkedList.java
@@ -8,5 +8,23 @@ public class TestLinkedList {
         System.out.println("Size of list: " + list.size());
         list.add(19);
         System.out.println("Size of list: " + list.size());
+
+        // expected: 5
+        System.out.println("element at index 0: " + list.get(0));
+
+        try {
+            list.get(2);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("OK");
+        }
+
+        try {
+            list.get(-1);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("OK");
+        }
+
+        System.out.println("list: " + list.toStringSlow());
+        System.out.println("faster toString(): " + list);
     }
 }
